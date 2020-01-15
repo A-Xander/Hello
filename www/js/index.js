af321a249f339b36fe0ea9b49daaf3ad95c1f8ca
Froese <cas301551@cas.dhbw.de>
2020-01-15 14:15:13 +0100

    service worker

--- www/js/index.js
+++ www/js/index.js
@@ -23,6 +23,7 @@ function bodyGeladen() {
 }
 
 function initApp() {
+  main();
   console.log("Starting framework7 app...");
   var app = new Framework7({
     root: "#app",
@@ -33,11 +34,38 @@ function initApp() {
     }
   });
 
-  if (window.usingCordova) {
-  } else {
-  }
-
   var mainView = app.views.create(".view-main");
 }
+
+//Prüfen ob Service Worker und Push API unterstützt werden
+const check = () => {
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No Service Worker support!");
+  }
+  if (!("PushManager" in window)) {
+    throw new Error("No Push API Support!");
+  }
+};
+
+//Den Service Worker im Browser unter Application --> Service Workers registrieren
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register("sw.js");
+  return swRegistration;
+};
+
+//Erlaubnis erfragen vom Benutzer, ob Push Nachrichten angezeigt werden dürfen.
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission();
+  if (permission !== "granted") {
+    throw new Error("Permission not granted for Notification");
+  }
+};
+
+//Funktion der Schaltfläche um die obrigen Funktionen aufzurufen
+const main = async () => {
+  check();
+  const swRegistration = await registerServiceWorker();
+  const permission = await requestNotificationPermission();
+};
 
 document.addEventListener("deviceready", initApp, false);
